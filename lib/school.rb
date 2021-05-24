d5e113e1800073a82982cbafbf42f41fdc6f7dd6
camryn williams <williams.camryn.e@gmail.com>
2021-05-24 18:50:47 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+
+        @roster[grade] << student
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster = @roster.each_key do | grade |
+            @roster[grade].sort!
+            # binding.pry
+            # @roster[grade].sort
+        end
+    end
+
+end
